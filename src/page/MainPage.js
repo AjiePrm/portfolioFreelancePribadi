4c1275a96592cd9ff0f6b1953db2f551f38ea3ab
ajieprima <ajiee.salahudin@gmail.com>
2023-04-14 09:30:36 +0700

    setup

---
+++ src/page/MainPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from '../part/Header'
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      </>
+    )
+  }
+}
